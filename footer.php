dc0601d1697f709668d50a6e19fb1b4a04cb1d86
Kvant.Development <development@kvant.io>
2020-03-26 10:11:39 +0000

    footer

---
+++ footer.php
@@ -0,0 +1,132 @@
+<footer class="footer" data-multiple-anim="fadeInUp">
+        <div class="footer__bg"></div>
+        <div class="container">
+          <div class="footer__top">
+            <div class="footer__item">
+              <h3 class="footer__title">О проекте</h3>
+              <ul class="footer__list">
+                <li>
+                  <a href="">White Paper</a>
+                </li>
+                 <li>
+                  <a href="">Скорость</a>
+                </li>
+                 <li>
+                  <a href="">Преимущества</a>
+                </li>
+
+                <li>
+                  <a href="">Hyperon</a>
+                </li>
+
+                <li>
+                  <a href="">Команда KVANT</a>
+                </li>
+
+
+              </ul>
+            </div>
+            <div class="footer__item">
+              <h3 class="footer__title">Сервисы</h3>
+              <ul class="footer__list">
+                <li>
+                  <a href="https://wallet.kvant.io/">KVANT Wallet</a>
+                </li>
+                <li>
+                  <a href="https://blockchain.kvant.io/">Обозреватель блокчейна</a>
+                </li>
+                <li>
+                  <a href="https://sale.kvant.io/">Купить KVANT</a>
+                </li>
+                <li>
+                  <a href="">Калькулятор Bitmining</a>
+                </li>
+                <li>
+                  <a href="">Модуль Bitmining</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer__item">
+              <h3 class="footer__title">Документы</h3>
+              <ul class="footer__list">
+                <li>
+                  <a href="">Политика конфиденциальности</a>
+                </li>
+                              </ul>
+              <ul class="footer__list footer__list_white footer__list_bottom">
+                <li>
+                  <a href="https://github.com/kvant-development/" target=github>GitHub</a>
+                </li>
+                <li>
+                  <a href="">Отказ от ответственности</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer__item">
+              <h3 class="footer__title">Контакты</h3>
+              <p class="footer__desc"><b>info@kvant.io</b> - по вопросам сотрудничества. </br>
+              <b>support@kvant.io</b> - служба технической поддержки. </br>
+              <b>press@kvant.io</b> - пресс-служба проекта. </p>
+              <div class="social social_type2">
+                <a class="social__item social__item_fb" href="">
+                  <div class="svg-icon svg-icon_icon-fb">
+                    <svg>
+                      <use xlink:href="#icon-fb"></use>
+                    </svg>
+                  </div>
+                </a>
+                <a class="social__item social__item_vk" href="">
+                  <div class="svg-icon svg-icon_icon-vk">
+                    <svg>
+                      <use xlink:href="#icon-vk"></use>
+                    </svg>
+                  </div>
+                </a>
+                <a class="social__item social__item_odn" href="">
+                  <div class="svg-icon svg-icon_icon-odn">
+                    <svg>
+                      <use xlink:href="#icon-odn"></use>
+                    </svg>
+                  </div>
+                </a>
+                <a class="social__item social__item_inst" href="">
+                  <div class="svg-icon svg-icon_icon-inst">
+                    <svg>
+                      <use xlink:href="#icon-inst"></use>
+                    </svg>
+                  </div>
+                </a>
+                <a class="social__item social__item_yt" href="">
+                  <div class="svg-icon svg-icon_icon-yt">
+                    <svg>
+                      <use xlink:href="#icon-yt"></use>
+                    </svg>
+                  </div>
+                </a>
+                <a class="social__item social__item_tg" href="">
+                  <div class="svg-icon svg-icon_icon-tg">
+                    <svg>
+                      <use xlink:href="#icon-tg"></use>
+                    </svg>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="footer__bottom">
+            <ul class="footer__copy-list">
+              <li>KVANT © 2020 Все права защищены</li>
+              <li>Копирование и использование информации запрещено.
+                
+              </li>
+                          </ul>
+            <div class="footer__logo">
+              <img src="/img/logo-footer.png" srcset="/img/logo-footer@2x.png 2x" alt="" />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  
